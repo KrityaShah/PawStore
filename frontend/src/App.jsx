718b97f264914609540a6d7e5c6459b206de0260
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-12-05 18:56:25 +0545

    Home section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 
+import Home from "./pages/Home"
+
 function App() {
   
 
   return (
     <>
-     <h1>Welcome to Paw Store</h1>
+
+    <Home/>
     </>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Home.css";
+import Navbar from "../components/Navbar";
+// Correctly import the image with its file extension
+import puppy from "../assets/puppy.png";
+import Dogbreed from "../components/Dogbreed";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="main-home-container">
+        <div className="left">
+          {/* Use the imported image variable */}
+          <img src={puppy} alt="Cute puppy" />
+          <div className="home-slider">
+            <button>left</button>
+            <p className="poppins-regular">Cargi (2months)</p>
+            <button>right</button>
+          </div>
+        </div>
+        <div className="right poppins-regular">
+        <h1>EVERYBODY NEEDS A FRIEND IN LIFE.</h1>
+        <p>
+          The Corgi is intelligent, quick and curious. It is a kind, adventurous
+          breed which shows a large measure of independence. They are good with
+          children and normally kind with strangers.
+        </p>
+        <div className="home-btn">
+          <button>Buy me</button>
+        </div> 
+        </div>
+       
+      </div>
+
+      <Dogbreed/>
+    </>
+  );
+};
+
+export default Home;

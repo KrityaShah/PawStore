2614c81ad97fc8885aaf3dd2fa15c3a5440b5624
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-12-05 19:40:09 +0545

    Updated Dogbreed

--- frontend/src/components/Dogbreed.jsx
+++ frontend/src/components/Dogbreed.jsx
@@ -1,38 +1,49 @@
 import React from "react";
-import "./Dogbreed.css"
+import "./Dogbreed.css";
+import gr from "../assets/gr.png";
+import sh from "../assets/sh.png";
+import pb from "../assets/pb.png";
+import gs from "../assets/gs.png";
+import pug from "../assets/pug.png";
+import js from "../assets/js.png";
+import labrador from "../assets/labrador.png";
+
 const Dogbreed = () => {
   return (
     <div className="main-dogbreed-container">
-      <h1>Dog Breed</h1>
-      <p>Find yourself a perfect friend from a wide variety of choices.</p>
+      <h1 className="poppins-bold">Dog Breed</h1>
+      <p className="poppins-regular">
+        Find yourself a perfect friend from a wide variety of choices.
+      </p>
+
       <div className="breeds">
         <div className="breed">
-            <img src="" alt="Golden Retriver" srcset="" />
-            <p>Golden Retriver</p>
+          <img src={gr} alt="Golden Retriver" srcset="" />
+          <p className="poppins-regularplus ">Golden Retriver</p>
         </div>
         <div className="breed">
-            <img src="Siberian Husky" alt="Siberian Husky" srcset="" />
-            <p>Siberian Husky</p>
+          <img src={sh} alt="Siberian Husky" srcset="" />
+          <p className="poppins-regularplus ">Siberian Husky</p>
         </div>
         <div className="breed">
-            <img src="" alt="Pitbull" />
-            <p>Pitbull</p>
+          <img src={pb} alt="Pitbull" />
+          <p className="poppins-regularplus ">Pitbull</p>
         </div>
         <div className="breed">
-            <img src="" alt="German Shepard" />
-            <p>German Shepard</p>
+          <img src={gs} alt="German Shepard" />
+          <p className="poppins-regularplus ">German Shepard</p>
         </div>
         <div className="breed">
-            <img src="" alt="Pug" />
-            <p>Pug</p>
+          <img src={pug} alt="Pug" />
+          <p className="poppins-regularplus ">Pug</p>
         </div>
         <div className="breed">
-            <img src="" alt="Japanese Spitz" />
-            <p>Japanese Spitz</p>
+          <img src={js} alt="Japanese Spitz" />
+          <p className="poppins-regularplus ">Japanese Spitz</p>
         </div>
         <div className="breed">
-            <img src="" alt="Labrador" />
-            <p>Labrador</p>
+          <img src={labrador} alt="Labrador" />
+          <p className="poppins-regularplus ">Labrador</p>
         </div>
       </div>
     </div>

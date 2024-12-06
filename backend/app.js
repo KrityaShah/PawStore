d2032fd706699301c0912f291f00453d8d2abaf4
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-12-06 11:53:59 +0545

    cors installation

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
 const router = require('./Router/auth-router')
@@ -7,6 +8,13 @@ const adminRouter = require('./Router/admin-router')
 const connectDb = require("./utils/db")
 
 
+const corsOptions = {
+    origin: "http://localhost:5173",
+    method: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+ };
+ 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", router);
 app.use("/admin/", adminRouter);
